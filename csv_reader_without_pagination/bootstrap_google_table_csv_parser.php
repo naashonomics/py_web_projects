d1fec1b7e40906379fd4331232c8004d1c076edd
Avinash Anantharamu <avinashgithub1988@gmail.com>
2020-11-17 21:20:33 -0800

    Update bootstrap_google_table_csv_parser.php

--- csv_reader_without_pagination/bootstrap_google_table_csv_parser.php
+++ csv_reader_without_pagination/bootstrap_google_table_csv_parser.php
@@ -20,7 +20,7 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>DAILY CL -VCS  </title>    
+    <title>TITLE   </title>    
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
